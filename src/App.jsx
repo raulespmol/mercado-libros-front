f7ddf6ebf4618d107ddafc37734336a4e713b257
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-02 17:04:40 -0400

    crear rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+//Components
 import Navbar from "./components/Navbar"
+import { Routes, Route } from 'react-router-dom'
+
+//Views
 import Home from "./views/Home"
+import NotFound from "./views/NotFound"
+
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Home/>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/libros" element={<Tienda />} />
+          <Route path="/libros/:id" element={<DetallePublicacion />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
     </>
   )
 }
---
+++ src/views/DetallePublicacion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetallePublicacion = () => {
+  return (
+    <div>DetallePublicacion</div>
+  )
+}
+
+export default DetallePublicacion
