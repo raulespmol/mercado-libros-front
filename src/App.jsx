70ac848c7a8fb99c1f261bef4902a134b69dea1b
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-02 16:52:34 -0400

    crear Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/Navbar"
 import Home from "./views/Home"
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>MercadoLibros</h1>
+      <Navbar/>
+      <Home/>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Navbar as BNav} from 'react-bootstrap';
 
-function NavbarPrueba() {
+function Navbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <BNav expand="lg" className="bg-success" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav className="me-auto">
+          <BNav.Brand href='#'>MercadoLibros</BNav.Brand> 
+        </Nav>
+        <Nav>
+          <Nav.Link>Registrar</Nav.Link>
+          <Nav.Link>Login</Nav.Link>
+          <Nav.Link>Carrito</Nav.Link>
+        </Nav>
       </Container>
-    </Navbar>
+    </BNav>
   );
 }
 
-export default NavbarPrueba;
+export default Navbar;
