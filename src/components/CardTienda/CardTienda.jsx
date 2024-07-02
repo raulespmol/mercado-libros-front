35d6e26a97f0170e67393d2aebb4c0f5db1c9653
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-02 18:13:59 -0400

    crear Card Tienda

---
+++ src/components/CardTienda/CardTienda.jsx
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './style.css'
+
+//valores por defecto para renderizar mientras se obtienen datos desde el back
+//el id de la publicacion se usara para navegar al detalle '/libros/:id'
+const CardTienda = ({id, titulo = "Titulo", autor = "Autor", precio = "$9.999", usuario="Raul Espinoza", imagen}) => {
+  return (
+    <Card style={{width: '360px'}}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <div>
+          <h5 className='titulo'>{titulo}</h5>
+          <span className='autor'>{autor}</span>
+        </div>
+        <p className='precio'>{precio}</p>
+      </Card.Body>
+      <Card.Footer>
+        <div>
+          <span>publicado por</span>
+          <p className='usuario'>{usuario}</p>
+        </div>
+        <Button variant="dark" size='sm'>Ver Detalles</Button>
+      </Card.Footer>
+    </Card>
+  )
+}
+
+export default CardTienda
+
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import { Container } from 'react-bootstrap'
+import CardTienda from '../components/CardTienda/CardTienda'
 
 const Home = () => {
   return (
-    <Hero></Hero>
+    <>
+      <Hero></Hero>
+      <Container>
+        <h2>Tienda</h2>
+        <CardTienda />
+      </Container>
+    </>
   )
 }
 
