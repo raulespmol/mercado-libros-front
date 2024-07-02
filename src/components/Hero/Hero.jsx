f60640bd8b6fb59d26fab6d90fc719061eeb160b
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-02 17:32:16 -0400

    crear Hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import { Button, Container } from "react-bootstrap"
+import './Hero.css'
+
+
+const Hero = () => {
+  return (
+    <Container fluid className="py-5">
+      <Container>
+        <p className="m-0">¡Dale una segunda vida a esos tesoros literarios!</p>
+        <h1>MercadoLibros</h1>
+        <Button variant="dark" size="lg">Tienda</Button>
+      </Container>
+    </Container>
+  )
+}
+
+export default Hero
