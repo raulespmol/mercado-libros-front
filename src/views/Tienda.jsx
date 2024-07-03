bceae7ae510646eea7ddb08fd07297b8e602f6d2
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-02 20:06:37 -0400

    Update Tienda.jsx
    maqueta de vista. Falta reemplazar el formulario por componente 'filtros'

--- src/views/Tienda.jsx
+++ src/views/Tienda.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import FormPublicacion from '../components/FormPublicacion/FormPublicacion'
+import CardTienda from '../components/CardTienda/CardTienda'
 
 const Tienda = () => {
   return (
-    <div>Tienda</div>
+    <Container fluid className='p-0 d-flex'>
+      <FormPublicacion /> {/* <--- CAMBIAR POR FILTROS */}
+      <div className='p-3'>
+        <h3 className='text-center'>Tienda</h3>
+        <Container>
+          <Row>
+            <Col sm={6} md={6}>
+              <CardTienda />
+            </Col>
+            <Col sm={6} md={6}>
+              <CardTienda />
+            </Col>
+            <Col sm={6} md={6}>
+              <CardTienda />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Container>
   )
 }
 
