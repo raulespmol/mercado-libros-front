c0e562c02634579c2eeb43c9b7108cc4dfa2d4cc
Yerko Browton <yerko.browton1995@hotmail.com>
2024-07-04 23:00:07 -0400

    Creación Login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import { Navbar as BNav } from 'react-bootstrap';
-import { NavLink } from "react-router-dom"
-import libro1 from '../../assets/img/logo-horizontal.png'
-import "./style.css"
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { Navbar as BNav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./style.css";
 
 function Navbar() {
-  const setActiveClass = ({isActive}) => isActive ? 'active' : ''
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : "");
 
   return (
     <BNav expand="lg" className="bg-success" variant="dark">
@@ -18,13 +17,28 @@ function Navbar() {
         </Nav>
         <Nav>
           <NavLink to="/registro" className={setActiveClass}>
-            Registrarse
+            <div className="d-flex text-white">
+              <div>
+                <i className="fa-solid fa-pen-to-square me-1"></i>
+              </div>
+              <div>Registrarse</div>
+            </div>
           </NavLink>
           <NavLink to="/login" className={setActiveClass}>
-            Login
+            <div className="d-flex text-white">
+              <div>
+                <i className="fa-solid fa-user me-1"></i>
+              </div>
+              <div>Ingresar</div>
+            </div>
           </NavLink>
           <NavLink to="/carrito" className={setActiveClass}>
-            Carrito
+            <div className="d-flex text-white">
+              <div>
+                <i className="fa-solid fa-bag-shopping me-1"></i>
+              </div>
+              <div>Carrito</div>
+            </div>
           </NavLink>
         </Nav>
       </Container>
--- src/views/Registro/Registro.jsx
+++ src/views/Registro/Registro.jsx
@@ -1,5 +1,7 @@
-import './style.css'
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
 import { useContext, useState } from "react";
+import "./style.css";
 // import { UserContext } from "../providers/UserProvider";
 
 const Registro = () => {
@@ -19,58 +21,53 @@ const Registro = () => {
 
   return (
     <div className="container-fluid bg-light d-flex justify-content-center p-5">
-      <div className="sectionMain bg-white p-5 m-5 border border-1 border-dark-subtle rounded-3">    
+      <div className="sectionMain bg-white p-5 border border-1 border-dark-subtle rounded-3">
         <div className="px-3 mx-4 mb-5">
           <h1>¡Te damos la bienvenida a Mercadolibros Chile!</h1>
           <h3>Crea tu cuenta y empieza a vender o comprar libros a miles de personas.</h3>
         </div>
         {/* <form onSubmit={handleSubmit}> */}
         <form>
-          <div className="mb-3">
-              <input
-                type="text"
-                id="nombre"
-                value={nombre}
-                onChange={(e) => setNombre(e.target.value)}
-                className="form-control"
-                placeholder="Nombre"
-              />
-          </div>
-          <div className="mb-3">
-            <input
+          <Form.Control
+            type="text"
+            id="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            className="form-control"
+            placeholder="Nombre"
+          />
+          <Form.Control
               type="email"
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="form-control"
               placeholder="Correo electrónico de contacto"
-            />
-          </div>
-          <div className="mb-3">
-            <input
+          />
+          <Form.Control
               type="password"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="form-control"
               placeholder="Contraseña"
-            />
-          </div>
-          <div className="mb-3">
-            <input
+          />
+          <Form.Control
               type="password"
               id="confirmarPassword"
               value={confirmarPassword}
               onChange={(e) => setConfirmarPassword(e.target.value)}
               className="form-control"
               placeholder="Confirma tu contraseña"
-            />
-          </div>
-          <button type="submit" className="btn btn-success rounded-5 fw-bold w-100">
+          />
+          <Button variant="success"  type="submit" className="w-100 rounded-5 fw-bold">
             Crear cuenta
-          </button>
-          <div className='goLogin mt-3'>
-            <p>¿Ya tienes cuenta? <a href="/login">inicia tu sesión</a></p>
+          </Button>
+          <hr></hr>
+          <div className="goLogin mt-3">
+            <p>
+              ¿Ya tienes cuenta? <a href="/login">inicia tu sesión</a>
+            </p>
           </div>
         </form>
       </div>
