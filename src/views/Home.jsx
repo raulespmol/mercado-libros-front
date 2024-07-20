b9779255cc80ca873a44aa7ab04225b88baa9e8b
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-19 22:45:01 -0400

    ajustes menores Home
    ajuestes de estilos en animacion y mostrar los primeros libros de la tienda

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
-import Hero from '../components/Hero/Hero'
-import { Container, Row, Col, Button } from 'react-bootstrap'
-import CardTienda from '../components/CardTienda/CardTienda'
 import { useNavigate } from "react-router-dom"
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { useContext } from 'react'
+import { LibrosContext } from '../context/LibrosContext'
+import Hero from '../components/Hero/Hero'
+import CardTienda from '../components/CardTienda/CardTienda'
 import SectionFeatures from '../components/SectionFeatures/SectionFeatures'
 
 const Home = () => {
   const navigate = useNavigate()
+  const { libros } = useContext(LibrosContext)
+  
+  const primerosLibros = libros.slice(0, 4)
 
   return (
     <>
       <Hero />
-    <SectionFeatures/>
+      <SectionFeatures/>
       <h2 className='text-center my-5 fw-semibold fs-1'>Tienda</h2>
       <Container>
         <Row>
-          <Col sm={6} md={4}>
-            <CardTienda />
-          </Col>
-          <Col sm={6} md={4}>
-            <CardTienda />
-          </Col>
-          <Col sm={6} md={4}>
-            <CardTienda />
-          </Col>
+          {primerosLibros.map(libro => (
+            <Col key={libro.libro_id} sm={12} md={4} lg={3} className="mb-2">
+              <CardTienda libro={libro} />
+            </Col>
+            ))
+          }
         </Row>
-        <Row className='my-3'>
-          <Button 
-            variant='success'
-            onClick={() => navigate('/libros')}
-          >
-            Ver más
-          </Button>
+        <Row className='mb-4'>
+          <Col>
+            <Button 
+              className="w-100"
+              variant="dark"
+              onClick={() => navigate('/libros')}
+            >
+              Ver más
+            </Button>
+          </Col>
         </Row>
 
       </Container>
