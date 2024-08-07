d125a8af4fd17e77eec012fb5337b6eb570278f6
Yerko Alejandro <79548300+YERKO09@users.noreply.github.com>
2024-08-07 17:41:31 -0400

    backend localhost apagado

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-export const URLBASE = import.meta.env.VITE_BASE_URL_LOCAL
-// export const URLBASE = import.meta.env.VITE_BASE_URL
+export const URLBASE = import.meta.env.VITE_BASE_URL
 
 export const ENDPOINT = {
   login: `${URLBASE}/login`,
